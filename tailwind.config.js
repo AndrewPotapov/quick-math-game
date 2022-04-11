d547d3b9dd5f09a9de54cd65e484fe583e9c9275
AndrewPotapov <unboxpros@gmail.com>
2022-04-11 16:56:57 +0200

    the basic list of colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   content: ["./src/index.html"],
-  theme: {},
+  theme: {
+    colors: {
+      "primary-container-light": "#FFDF8B",
+      "primary-container-dark": "#594400",
+      "primary-light": "#765B00",
+      "primary-dark": "#ECC142",
+      dark: "#1E1E1E",
+      light: "#FFFBF7",
+    },
+  },
   plugins: [],
 };
